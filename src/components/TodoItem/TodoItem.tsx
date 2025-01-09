4e2bffc87d6d54538855c0e1bf52f46ad7b1dcb5
HATTORIULTRA <throwbvck@gmail.com>
2025-01-09 21:30:26 +0500

    prod: ver 0.1

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,16 +1,14 @@
-import {FC, ReactNode, useState} from "react";
-import {FaPenToSquare, FaTrashCan, FaCheck, FaXmark} from "react-icons/fa6";
-import s from './TodoItem.module.scss';
+import { FC, useState } from "react";
+import { FaPenToSquare, FaTrashCan, FaCheck, FaXmark } from "react-icons/fa6";
+import s from "./TodoItem.module.scss";
 
 interface TodoItemProps {
    id: number;
    title: string;
    isDone: boolean;
    deleteTodo: (id: number) => void;
-   editTodoText: (id: number) => void;
+   editTodoText: (id: number, newValue: string) => void;
    completeTodo: (id: number) => void;
-   editedTodoValue: string;
-   setEditedTodoValue: (value: string) => void;
    todoIdForEdit: number | null;
    setTodoIdForEdit: (id: number | null) => void;
 }
@@ -22,20 +20,20 @@ const TodoItem: FC<TodoItemProps> = ({
                                         deleteTodo,
                                         completeTodo,
                                         editTodoText,
-                                        editedTodoValue,
-                                        setEditedTodoValue,
                                         todoIdForEdit,
                                         setTodoIdForEdit,
-                                     }): ReactNode => {
-   const [error, setError] = useState<string>('');
+                                     }) => {
+   const [localEditedValue, setLocalEditedValue] = useState<string>(title);
+   const [error, setError] = useState<string>("");
    const isEditing: boolean = todoIdForEdit === id;
 
    const onEditClick = (): void => {
       setTodoIdForEdit(id);
+      setLocalEditedValue(title);
    };
 
    const onAcceptClick = (): void => {
-      const trimValue = editedTodoValue.trim();
+      const trimValue = localEditedValue.trim();
 
       if (trimValue.length < 2 || trimValue.length > 64) {
          setError("Задача должна содержать от 2 до 64 символов.");
@@ -49,21 +47,14 @@ const TodoItem: FC<TodoItemProps> = ({
          return;
       }
       setError("");
-      editTodoText(id);
+      editTodoText(id, trimValue);
       setTodoIdForEdit(null);
    };
 
    const onCancelClick = (): void => {
-      setEditedTodoValue("");
       setError("");
       setTodoIdForEdit(null);
    };
-
-   const onBlurHandler = (e: any) => {
-      if(e.target && e.target.value.length < 2) {
-         setError("Поле должно быть заполнено");
-      }
-   }
 
    const completedStyles = {
       color: "darkgrey",
@@ -82,9 +73,8 @@ const TodoItem: FC<TodoItemProps> = ({
          {isEditing ? (
             <div className={s.editFormWrapper}>
                <input
-                  onBlur={(e) => onBlurHandler(e)}
-                  value={editedTodoValue}
-                  onChange={(e) => setEditedTodoValue(e.target.value)}
+                  value={localEditedValue}
+                  onChange={(e) => setLocalEditedValue(e.target.value)}
                   className={s.editInput}
                   type="text"
                   placeholder="new text"
